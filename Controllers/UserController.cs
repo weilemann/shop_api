20aa3f34c4b8753050b0080bd9beab8287f7cfbc
Yuri Weilemann <t2m.yweilemann@terceiros.mag.com.br>
2020-12-16 16:26:20 -0300

    Project concluded

---
+++ Controllers/UserController.cs
@@ -0,0 +1,107 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+using Shop.Services;
+
+namespace Shop.Controllers
+{
+    [Route("v1/users")]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        [Authorize(Roles = "manager")]
+        [ResponseCache(VaryByHeader = "User-Agent", Location = ResponseCacheLocation.Any, Duration = 30)]
+        public async Task<ActionResult<List<User>>> GetAction(
+            [FromServices] DataContext context)
+        {
+            var users = await context.Users.AsNoTracking().ToListAsync();
+            
+            return users;
+        }
+
+
+        [HttpPost]
+        [Route("")]
+        [AllowAnonymous]
+        public async Task<ActionResult<User>> Post(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            try
+            {
+                model.Role = "employee";
+
+                context.Users.Add(model);
+                await context.SaveChangesAsync();
+
+                model.Password = "";
+                return model;
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível criar o usuário" });
+            }
+        }
+
+        [HttpPut]
+        [Route("{id:int}")]
+        [Authorize(Roles = "manager")]
+        public async Task<ActionResult<User>> Put(
+            [FromServices] DataContext context,
+            int id,
+            [FromBody] User model)
+        {
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            if(id != model.Id)
+                return NotFound(new { message = "Usuário não encontrado" });
+
+            try
+            {
+                context.Entry(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return model;
+            }
+            catch(Exception)
+            {
+                return BadRequest(new { message = "Não foi possível alterar informações do usuário" });
+            }
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<ActionResult<dynamic>> Authenticate(
+            [FromServices] DataContext context,
+            [FromBody] User model)
+        {
+            var user = await context.Users
+                .AsNoTracking()
+                .Where(x => x.Username == model.Username && x.Password == model.Password)
+                .FirstOrDefaultAsync();
+
+            if(user == null)
+                return NotFound(new { message = "Usuário ou senha inválidos" });
+
+            var token = TokenService.GenerateToken(user);
+
+            user.Password = "";
+
+            return new
+            {
+                user = user,
+                token = token
+            };
+        }
+    }
+}
---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "150f460159418940a68b719ac48e86ac";
+    }
+}
